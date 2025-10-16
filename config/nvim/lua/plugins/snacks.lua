05060310678602a7ebe613227c4cfe5da1e14b3e
Azzam <25992629+azzamalthagafi@users.noreply.github.com>
2025-10-15 23:48:53 -0400

    better

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -8,16 +8,26 @@ return {
             return { { "[", hl = "special" }, { item.key, hl = "key" }, { "]", hl = "special" } }
           end,
         },
+
         sections = {
-          { section = "terminal", cmd = "fortune -s | cowsay", hl = "header", padding = 1, indent = 8 },
-          { title = "MRU", padding = 1 },
-          { section = "recent_files", limit = 8, padding = 1 },
-          { title = "MRU ", file = vim.fn.fnamemodify(".", ":~"), padding = 1 },
-          { section = "recent_files", cwd = true, limit = 8, padding = 1 },
-          { title = "Sessions", padding = 1 },
-          { section = "projects", padding = 1 },
-          { title = "Bookmarks", padding = 1 },
-          { section = "keys" },
+          { section = "keys", gap = 1, padding = 1 },
+          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          {
+            pane = 2,
+            icon = " ",
+            title = "Git Status",
+            section = "terminal",
+            enabled = function()
+              return Snacks.git.get_root() ~= nil
+            end,
+            cmd = "git status --short --branch --renames",
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          },
+          { section = "startup" },
         },
       },
     },
