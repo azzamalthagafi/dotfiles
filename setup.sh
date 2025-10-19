fd8666339494787f90a35eeffddfa6d032820bb0
Azzam <25992629+azzamalthagafi@users.noreply.github.com>
2025-10-19 11:30:25 -0400

    tmux

--- setup.sh
+++ setup.sh
@@ -10,4 +10,10 @@ ln -Fs "$PWD/config/nvim/" ~/.config/nvim/
 # Link .tmux.conf
 ln -s "$PWD/tmux/.tmux.conf" ~/.tmux.conf
 
+# Clone tmux plugin manager if it doesn't exist
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+    echo "Cloning tmux plugin manager..."
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
 echo "Dotfiles linked successfully!"
