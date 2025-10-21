22f09fb47a057f2b03af3cf34c3272588335a540
Azzam <25992629+azzamalthagafi@users.noreply.github.com>
2025-10-21 07:10:45 -0400

    python + tmux-powerline

--- setup.sh
+++ setup.sh
@@ -3,12 +3,16 @@
 # Create directories if they don't exist
 mkdir -p ~/.config/nvim
 mkdir -p ~/.local/bin
+mkdir -p ~/.config/tmux-powerline
 
 # Link nvim config files
 ln -Fs "$PWD/config/nvim/" ~/.config/nvim/
 
 # Link .tmux.conf
 ln -s "$PWD/tmux/.tmux.conf" ~/.tmux.conf
+
+# Link tmux-powerline config
+ln -s "$PWD/tmux/tmux-powerline-config.sh" ~/.config/tmux-powerline/config.sh
 
 # Clone tmux plugin manager if it doesn't exist
 if [ ! -d ~/.tmux/plugins/tpm ]; then
