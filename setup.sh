572c0b90fd95aeb48a1a64c84a13d43a50e2fadd
Azzam <25992629+azzamalthagafi@users.noreply.github.com>
2025-10-21 07:46:57 -0400

    setup tmux-powerline

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@
 mkdir -p ~/.config/nvim
 mkdir -p ~/.local/bin
 mkdir -p ~/.config/tmux-powerline
+mkdir -p ~/.config/tmux-powerline/themes
 
 # Link nvim config files
 ln -Fs "$PWD/config/nvim/" ~/.config/nvim/
@@ -12,7 +13,10 @@ ln -Fs "$PWD/config/nvim/" ~/.config/nvim/
 ln -s "$PWD/tmux/.tmux.conf" ~/.tmux.conf
 
 # Link tmux-powerline config
-ln -s "$PWD/tmux/tmux-powerline-config.sh" ~/.config/tmux-powerline/config.sh
+ln -fs "$PWD/tmux/tmux-powerline-config.sh" ~/.config/tmux-powerline/config.sh
+
+# Link tmux-powerlinne theme
+ln -fs "$PWD/tmux/tmux-powerline-theme.sh" ~/.config/tmux-powerline/themes/tmux-theme.sh
 
 # Clone tmux plugin manager if it doesn't exist
 if [ ! -d ~/.tmux/plugins/tpm ]; then
